069fc5729e953100810f6a3249ce22238065c9ae
jordanrob709 <jordanrob709@gmail.com>
2020-07-11 23:54:03 +0300

    added student middleware

--- satbackend/server.js
+++ satbackend/server.js
@@ -3,12 +3,17 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const studentUrls = require('./api/student')
 
 dotenv.config()
 
 mongoose.connect(process.env.DB_ACCESS, { useUnifiedTopology: true }, () => {
     console.log('DB Connected')
 })
+
+app.use(express.json())
+
+app.use('/students', studentUrls)
 
 app.listen(5000, () => {
     console.log('server is up & running')
